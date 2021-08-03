a5dbe7d3c90cb4500f828e2db04b9156b94c9cf5
GianmarcoMulaz <55657424+GianmarcoMU@users.noreply.github.com>
2021-08-03 18:29:51 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,5 +1,4 @@
-### Here we try to create an interface 
-## Note that this code just defines the graphical aspects, any functionality has been defined yet.
+### Here we try to create an interface starting from the original documentation about tkinter
 
 # Firstly, import the required library. Namely, tkinter
 import tkinter as ttk
@@ -10,94 +9,77 @@ root = ttk.Tk()
 
 # Start adding some features to it
 root.title("Option Calculator")
-root.geometry("800x700")
 root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight = 1)
 
 #Create frames to organise better the interface
 mainframe = ttk.Frame(root)
-frame2 = ttk.Frame(mainframe, borderwidth = 5, relief = "ridge")
-frame3 = ttk.Frame(mainframe, borderwidth = 5, relief = "ridge")
-frame4 = ttk.Frame(mainframe, borderwidth = 5, relief = "ridge")
+frame1 = ttk.Frame(mainframe, borderwidth = 5, relief = 'groove')
+frame2 = ttk.Frame(mainframe, borderwidth = 5, relief = "groove")
+frame3 = ttk.Frame(mainframe, borderwidth = 5, relief = "groove")
+frame4 = ttk.Frame(mainframe, borderwidth = 5, relief = "groove")
+
+# Create a Welcome text in the first frame to explain the functionalities of the application
+welcome = ttk.Label(frame1, text = "Welcome to the Option Calculator! This application has been developed to compute the theoretical price of option\
+derivatives using different methods \nfor given various inputs. Before using it make sure to use the british\
+notation (dot) for decimals and specify the dates in the following format.").grid(row = 0, columnspan = 4, sticky = "W")
 
 #Create some labels to make the interface clearer
-opt_type = ttk.Label(frame2, text = "Option Type:", font = ('Helvetica', 10, 'bold'))
-ex_style = ttk.Label(frame2, text = "Exercise Style:", font = ('Helvetica', 10, 'bold'))
-meth = ttk.Label(frame2, text = "Methodology:", font = ('Helvetica', 10, 'bold'))
-str_pr_l = ttk.Label(frame3, text = "Strike Price", font = ('Helvetica', 10, 'bold'))
-st_pr_l = ttk.Label(frame3, text = "Stock Price", font = ('Helvetica', 10, 'bold'))
-div_l = ttk.Label(frame3, text = "Dividend Yield", font = ('Helvetica', 10, 'bold'))
-start_l = ttk.Label(frame3, text = "Start Date", font = ('Helvetica', 10, 'bold'))
-exp_l = ttk.Label(frame3, text = "Expiration Date", font = ('Helvetica', 10, 'bold'))
-vol_l = ttk.Label(frame3, text = "Volatility", font = ('Helvetica', 10, 'bold'))
-rate_l = ttk.Label(frame3, text = "Interest Rate", font = ('Helvetica', 10, 'bold'))
+opt_type = ttk.Label(frame2, text = "Option Type:", font = ('Helvetica', 10, 'bold')).grid(row = 0, column = 0, sticky = "W", padx = 5, pady = 5)
+ex_style = ttk.Label(frame2, text = "Exercise Style:", font = ('Helvetica', 10, 'bold')).grid(row = 1, column = 0, sticky = "W", padx = 5, pady = 5)
+meth = ttk.Label(frame2, text = "Methodology:", font = ('Helvetica', 10, 'bold')).grid(row = 2, column = 0, sticky = "W", padx = 5, pady = 5)
+str_pr_l = ttk.Label(frame2, text = "Strike Price", font = ('Helvetica', 10, 'bold')).grid(row = 3, column = 0, sticky = "W", padx = 5, pady = 5)
+st_pr_l = ttk.Label(frame2, text = "Stock Price", font = ('Helvetica', 10, 'bold')).grid(row = 3, column = 2, sticky = "W", padx = 5, pady = 5)
+start_l = ttk.Label(frame2, text = "Start Date", font = ('Helvetica', 10, 'bold')).grid(row = 4, column = 0, sticky = "W", padx = 5, pady = 5)
+exp_l = ttk.Label(frame2, text = "Expiration Date", font = ('Helvetica', 10, 'bold')).grid(row = 4, column = 2, sticky = "W", padx = 5, pady = 5)
+vol_l = ttk.Label(frame2, text = "Volatility", font = ('Helvetica', 10, 'bold')).grid(row = 5, column = 0, sticky = "W", padx = 5, pady = 5)
+rate_l = ttk.Label(frame2, text = "Interest Rate", font = ('Helvetica', 10, 'bold')).grid(row = 5, column = 2, sticky = "W", padx = 5, pady = 5)
+div_l = ttk.Label(frame2, text = "Dividend Yield", font = ('Helvetica', 10, 'bold')).grid(row = 5, column = 4, sticky = "W", padx = 5, pady = 5)
 
 # Create some useful buttons
-put = ttk.Checkbutton(frame2, text = "Put")
-call = ttk.Checkbutton(frame2, text = "Call")
-american = ttk.Checkbutton(frame2, text = "American")
-european = ttk.Checkbutton(frame2, text = "European")
-bs = ttk.Checkbutton(frame2, text = "Black-Scholes")
-bin = ttk.Checkbutton(frame2, text = "Binomial Tree")
-jdm = ttk.Checkbutton(frame2, text = "Jump Diffusion")
-
-#Position labels and buttons accordingly
-opt_type.grid(row = 0, column = 0, sticky = "W", padx = 5, pady = 5)
-put.grid(row = 0, column = 1, sticky = "W")
-call.grid(row = 0, column = 2, sticky = "W")
-
-ex_style.grid(row = 1, column = 0, sticky = "W", padx = 5, pady = 5)
-american.grid(row = 1, column = 1, sticky = "W")
-european.grid(row = 1, column = 2, sticky = "W")
-
-meth.grid(row = 2, column = 0, sticky = "W", padx = 5, pady = 5)
-bs.grid(row = 2, column = 1, sticky = "W")
-bin.grid(row = 2, column = 2, sticky = "W")
-jdm.grid(row = 2, column = 3, sticky = "W")
+put = ttk.Checkbutton(frame2, text = "Put").grid(row = 0, column = 1, sticky = "W")
+call = ttk.Checkbutton(frame2, text = "Call").grid(row = 0, column = 2, sticky = "W")
+american = ttk.Checkbutton(frame2, text = "American").grid(row = 1, column = 1, sticky = "W")
+european = ttk.Checkbutton(frame2, text = "European").grid(row = 1, column = 2, sticky = "W")
+bs = ttk.Checkbutton(frame2, text = "Black-Scholes").grid(row = 2, column = 1, sticky = "W")
+bin = ttk.Checkbutton(frame2, text = "Binomial Tree").grid(row = 2, column = 2, sticky = "W")
+jdm = ttk.Checkbutton(frame2, text = "Jump Diffusion").grid(row = 2, column = 3, sticky = "W")
 
 # Define variables that must be inserted manually
-str_pr = ttk.Entry(frame3)
-st_pr = ttk.Entry(frame3)
-start = ttk.Entry(frame3)
-exp = ttk.Entry(frame3)
-vol = ttk.Entry(frame3)
-rate = ttk.Entry(frame3)
-div = ttk.Entry(frame3)
-
-#Let organise the second frame with other entry variables
-str_pr_l.grid(row = 3, column = 0, sticky = "W", padx = 5, pady = 5)
-str_pr.grid(row = 3, column = 1)
-
-st_pr_l.grid(row = 3, column = 2, sticky = "W", padx = 5, pady = 5)
-st_pr.grid(row = 3, column = 3)
-
-start_l.grid(row = 4, column = 0, sticky = "W", padx = 5, pady = 5)
-start.grid(row = 4, column = 1)
-
-exp_l.grid(row = 4, column = 2, sticky = "W", padx = 5, pady = 5)
-exp.grid(row = 4, column = 3)
-
-vol_l.grid(row = 5, column = 0, sticky = "W", padx = 5, pady = 5)
-vol.grid(row = 5, column = 1)
-
-rate_l.grid(row = 5, column = 2, sticky = "W", padx = 5, pady = 5)
-rate.grid(row = 5, column = 3)
-
-div_l.grid(row = 5, column = 4, sticky = "W", padx = 5, pady = 5)
-div.grid(row = 5, column = 5)
+str_pr = ttk.Entry(frame2).grid(row = 3, column = 1)
+st_pr = ttk.Entry(frame2).grid(row = 3, column = 3)
+start = ttk.Entry(frame2).grid(row = 4, column = 1)
+exp = ttk.Entry(frame2).grid(row = 4, column = 3)
+vol = ttk.Entry(frame2).grid(row = 5, column = 1)
+rate = ttk.Entry(frame2).grid(row = 5, column = 3)
+div = ttk.Entry(frame2).grid(row = 5, column = 5)
 
 #Create two buttons that calculate outputs and reset inputs
-calc = ttk.Button(frame4, text = "CALCULATE")
-reset = ttk.Button(frame4, text = "RESET")
+calc = ttk.Button(frame3, text = "CALCULATE").grid(row = 0, column = 0, padx = 160, pady = 5)
+reset = ttk.Button(frame3, text = "RESET").grid(row = 1, column = 0)
+plot = ttk.Button(frame3, text = "PLOT").grid(row = 2, column = 0, pady = 5)
 
-calc.grid(row = 0, column = 2, padx = 150)
-reset.grid(row = 0, column = 3)
+# Define outputs: both labels and entries
+th_price_l = ttk.Label(frame4, text = "Theoretical Price").grid(row = 0, column = 0, sticky = "W")
+delta_l = ttk.Label(frame4, text = "Delta").grid(row = 0, column = 2, sticky = "W")
+rho_l = ttk.Label(frame4, text = "Rho").grid(row = 1, column = 0, sticky = "W")
+gamma_l = ttk.Label(frame4, text = "Gamma").grid(row = 1, column = 2, sticky = "W")
+theta_l = ttk.Label(frame4, text = "Theta").grid(row = 2, column = 0, sticky = "W")
+vega_l = ttk.Label(frame4, text = "Vega").grid(row = 2, column = 2, sticky = "W")
 
+th_price = ttk.Entry(frame4).grid(row = 0, column = 1)
+delta = ttk.Entry(frame4).grid(row = 0, column = 3)
+rho = ttk.Entry(frame4).grid(row = 1, column = 1)
+gamma = ttk.Entry(frame4).grid(row = 1, column = 3)
+theta = ttk.Entry(frame4).grid(row = 2, column = 1)
+vega = ttk.Entry(frame4).grid(row = 2, column = 3)
 
 #Organise frames in the root
 mainframe.grid(row = 0, column = 0)
-frame2.grid(row = 0, sticky = "EW")
-frame3.grid(row = 1, sticky = "EW")
-frame4.grid(row = 2, sticky = "EW")
+frame1.grid(row = 0, columnspan = 2, sticky = "EW")
+frame2.grid(row = 1, columnspan = 2, sticky = "EW")
+frame3.grid(row = 2, column = 0, sticky = "EW")
+frame4.grid(row = 2, column = 1,  sticky = "EW")
 
 # This line of code open the window created
 root.mainloop()
