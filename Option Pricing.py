919ba7ecc0e7ee00325d72b8de33b05157cb5f88
GianmarcoMulaz <55657424+GianmarcoMU@users.noreply.github.com>
2021-08-06 22:42:16 +0200

    Add files via upload

---
+++ Option Pricing.py
@@ -0,0 +1,11 @@
+### This script is intended to develop the function to compute option pricing
+### it should be then included in the main code of the Option Calculator
+
+# Firstly, define the variables
+# We need: strike price, stock price, interest rate, dividends, maturity, volatility
+
+# We need Numpy to use statistical functions
+import numpy as np
+
+
+
