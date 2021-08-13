6dbf01f76f50a65acc8f13a4e32ff0d68066f811
GianmarcoMulaz <55657424+GianmarcoMU@users.noreply.github.com>
2021-08-13 18:39:48 +0200

    Update Binomial Model.py

--- Binomial Model.py
+++ Binomial Model.py
@@ -1,49 +1,81 @@
-import tkinter as ttk
-from tkinter import *
-import scipy as sp
-from scipy import stats
-from scipy.stats import norm
 import numpy as np
+from numpy.lib.function_base import percentile
 
 # Firstly, we define the variables needed to implement the lattice discretization
-steps = 500
-#delta_time = T/steps
-#u = np.exp(sigma*np.sqrt(delta_time))
-#d = 1/u
-#q = (1/(u-d))*(np.exp(r*delta_time)-d)
-
+steps = 2
+T = 1
 S = 10
 K = 10
-u = 1.2
-d = 0.8
-q = 0.75
+delta_time = T/steps
+sigma = 0.3
 rate = 0.1
+u = np.exp(sigma*np.sqrt(delta_time))
+d = 1/u
+q_rn = (1/(u-d))*(np.exp(rate*delta_time)-d)
+qq = 1-q_rn
+dis = 1/(1+rate)
 
-row = 3
-col = 3
+row = steps+1
+col = steps+1
 
 # We initialize an empty matrix that will be populated recursively
-lattice_S = np.zeros((row, col))
-value_process = np.zeros((row, col))
-CV_process = np.zeros((row, col))
-lattice_S[0, 0] = S
+
+lattice = np.zeros((row, col))
+payoff = np.zeros((row, col))
+price = np.zeros((row, col))
+lattice[0, 0] = S
 
 for i in range(1, row, 1):
     for j in range(1, col, 1):
-        lattice_S[0, j] = S*(u**j)
-        lattice_S[i, j] = lattice_S[i-1, j-1]*d
+        lattice[0, j] = S*(u**j)
+        lattice[i, j] = lattice[i-1, j-1]*d
 
-for r in range(0, row, 1):
-    for c in range(0, col, 1):
-        value_process[r, c] = max(K-lattice_S[r, c], 0)
+for i in range(0, row, 1):
+    for j in range(0, col, 1):
+        if lattice[i, j] == 0:
+            payoff[i, j] = 0
+        else:
+            payoff[i, j] = max(K-lattice[i, j], 0)
 
-# Check this process, something goes wrong
-for r in range(row-1, -1, -1):
-    for c in range(col-1, -1, -1):
-        CV_process[r-1, c-1] = (1/(1+rate))*(q*value_process[r, c+1]+(0.25)*value_process[r+1,c+1])
-        
-    
-print(lattice_S)
-print(value_process)
-print(CV_process)
+for i in range(row-1, -1, -1):
+    for j in range(col-1, -1, -1):
+        if lattice[i, j] ==0:
+            price[i, j] = 0
+        else:
+            if j == col-1:
+                price[i, j] = payoff[i, j]
+            else:
+                price[i, j] = max(payoff[i, j], dis*(q_rn*price[i, j+1]+qq*price[i+1, j+1]))
+                    
+print(price)
+
+lattice = np.zeros((row, col))
+payoff = np.zeros((row, col))
+price = np.zeros((row, col))
+lattice[0, 0] = S
+
+for i in range(1, row, 1):
+    for j in range(1, col, 1):
+        lattice[0, j] = S*(u**j)
+        lattice[i, j] = lattice[i-1, j-1]*d
+
+for i in range(0, row, 1):
+    for j in range(0, col, 1):
+        if lattice[i, j] == 0:
+            payoff[i, j] = 0
+        else:
+            payoff[i, j] = max(K-lattice[i, j], 0)
+
+for i in range(row-1, -1, -1):
+    for j in range(col-1, -1, -1):
+        if lattice[i, j] == 0:
+            price[i, j] = 0
+        else:
+            if j == col-1:
+                price[i, j] = payoff[i, j]
+            else:
+                price[i, j] = max(payoff[i, j], dis*(q_rn*price[i, j+1]+qq*price[i+1, j+1]))
+c0 = price[0, 0]
+
+print(c0)
 
